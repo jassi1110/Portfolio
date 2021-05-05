1d2a00f43911dcb2320472850a4d02df5fc21671
jassi1110 <jasnoorsingh11@gmail.com>
2021-05-06 00:01:08 +0530

    updated

--- hello.js
+++ hello.js
@@ -1,12 +1,26 @@
-console.log("Hello")
-window.onscroll = function() {myFunction()};
-// var navbar = document.getElementById("Jassi");
+// For Sticky Navbar
+window.onscroll = function () { myFunction() };
 var navbar = document.querySelector(`.navigation`)
 var sticky = navbar.offsetTop;
 function myFunction() {
     if (window.pageYOffset >= 40) {
-      navbar.classList.add("sticky")
+        navbar.classList.add("sticky")
     } else {
-      navbar.classList.remove("sticky");
+        navbar.classList.remove("sticky");
     }
-  }
+} 
+
+// For highlighting section in the Navbar
+const sections = document.querySelectorAll(`section`)
+const navLi = document.querySelectorAll(`.navbar .butns`)
+
+window.addEventListener('scroll' ,()=>{
+let current = ''
+sections.forEach(section =>{
+    const secTop = section.offsetTop
+    if(pageYOffset>secTop){
+        current = section.getAttribute(`id`)
+    }
+})
+console.log(current)
+})
